f67a8c7410da0e249c1e9adb9f974c888ad0aedb
Ivan-Costa <ivan90vitoria@gmail.com>
2026-03-17 16:59:17 +0100

    Delete Ejercicio Práctico.py

--- Ejercicio Práctico.py
+++
@@ -1,9 +0,0 @@
-# Cree una clase de Python llamada Usuario que use el método init y cree un nombre 
-# de usuario y una contraseña. Crea un objeto usando la clase.
-class Usuario:
-    def __init__(self, user, password):
-        self.user = user
-        self.password = password
-
-
-usuario_uno = Usuario('navyblue', 'contraseña')
